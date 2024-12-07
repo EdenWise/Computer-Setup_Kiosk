0b6925da3eeecc94e94b5a6312e4c0a6826c69f5
Todd Partridge <toddpartridge@outlook.com>
2024-12-06 17:09:32 -0800

    Add files via upload

--- CSK-Download.ps1
+++ CSK-Download.ps1
@@ -1,6 +1,6 @@
 $URIS = @(
   "https://7-zip.org/a/7zr.exe"  ## ???
-  "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/master/7za.exe"
+  #"https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/master/7za.exe"
   #
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/master/Archive-Extract.ps1"
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/master/LinksVariables-Create.ps1"

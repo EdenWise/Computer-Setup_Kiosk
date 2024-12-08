84c8c0aaf4976cc5302631c18a0e4b6b9b7aa302
Todd Partridge <toddpartridge@outlook.com>
2024-12-08 14:04:51 -0800

    Add files via upload

--- pwsh-update.ps1
+++ pwsh-update.ps1
@@ -1,13 +1,13 @@
 ## Scoop requires applications `pwsh` and `windows-terminal` to be updated with PowerShell 5.1.
 #
 if ( Get-Process -Name OpenConsole ) {
-  # Stop-Process -Name OpenConsole
-  echo "Stop-Process -Name OpenConsole"
+  # echo "Stop-Process -Name OpenConsole"
+  Stop-Process -Name OpenConsole
   exit
 }
-if ( Get-Process -Name OpenConsole ) {
-  # Stop-Process -Name WindowsTerminal
+if ( Get-Process -Name WindowsTerminal ) {
   echo "Stop-Process -Name WindowsTerminal"
+  # Stop-Process -Name WindowsTerminal
   exit
 }
 conhost.exe powershell.exe -ExecutionPolicy Unrestricted -NoExit -NoProfile -Command "$env:USERPROFILE\Program-Manager\apps\scoop\current\bin\scoop.ps1 update cascadia-code,pwsh,windows-terminal"

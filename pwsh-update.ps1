391c249dff3d5693566cadbf17c7591751c14287
Todd Partridge <toddpartridge@outlook.com>
2024-12-09 12:09:17 -0800

    .

--- pwsh-update.ps1
+++ pwsh-update.ps1
@@ -1,11 +1,11 @@
 ## Scoop requires applications `pwsh` and `windows-terminal` to be updated with PowerShell 5.1.
 #
-if ( Get-Process -Name OpenConsole ) {
+if ( Get-Process -Name OpenConsole -ErrorAction SilentlyContinue ) {
   # echo "Stop-Process -Name OpenConsole"
   Stop-Process -Name OpenConsole
   exit
 }
-if ( Get-Process -Name WindowsTerminal ) {
+if ( Get-Process -Name WindowsTerminal -ErrorAction SilentlyContinue ) {
   echo "Stop-Process -Name WindowsTerminal"
   # Stop-Process -Name WindowsTerminal
   exit

90139035b83686abdfe896bd24084da1b0e4cfc3
Todd Partridge <toddpartridge@outlook.com>
2024-12-11 16:02:28 -0800

    .

--- Computer-Setup.ps1
+++ Computer-Setup.ps1
@@ -108,8 +108,9 @@ foreach ( $fnt in $FNT_LST ) {
     if ( -not ( Test-Path "$env:LOCALAPPDATA\Microsoft\Windows\Fonts" ) ) {
       mkdir "$env:LOCALAPPDATA\Microsoft\Windows\Fonts"
     }
-    cp $fnt.FullName "$env:LOCALAPPDATA\Microsoft\Windows\Fonts"
-    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "$FNT_NME (TrueType)" -PropertyType String -Value "$env:LOCALAPPDATA\Microsoft\Windows\Fonts\$fnt.Name" -Force
+    # cp $fnt.FullName "$env:LOCALAPPDATA\Microsoft\Windows\Fonts"  ## Hardli
+    # New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "$FNT_NME (TrueType)" -PropertyType String -Value "$env:LOCALAPPDATA\Microsoft\Windows\Fonts\$fnt.Name" -Force | Select-Object -ExpandProperty Name
+    #regfont.exe $fnt.FullName
   }
 }
 

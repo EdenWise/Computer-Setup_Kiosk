e84cf097a804a84fe0b1a43d91c00b0c79d71772
Todd Partridge <toddpartridge@outlook.com>
2024-12-14 12:32:47 -0800

    Add files via upload

--- CSK-Download.ps1
+++ CSK-Download.ps1
@@ -1,8 +1,6 @@
 ## Computer Setup on a Kiosk, download scripts.
-### curl.exe https://bit.ly/csk-d -Lo CSK-Download.ps1
 ### "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/master/7za.exe"
 ### "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/Archive-Extract.ps1"
-### PSScriptAnalyzer: https://devblogs.microsoft.com/powershell/using-psscriptanalyzer-to-check-powershell-version-compatibility/
 
 $URIS = @(
   "https://7-zip.org/a/7zr.exe"
@@ -20,4 +18,5 @@ foreach ( $uri in $URIS ) {
   # Start-BitsTransfer -Asyncronous $uri
 }
 
-Write-Output "curl.exe `"https://ice-eu-53588.icedrive.io/download?p=hFmMquZjF679SnFTLVMImJ5KVht6jTuSMt56Gmr8g1Fc4iOiSnL_DdrdG2OyxdwiKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWPtr0bhldDjG_OCD2Ka2foV67oRTocxCVmXxYw3gfoqqFZFcva8ZNGbmSp5zAhENnRNtkit57.qm0dw7wXclI25Pm6DP5HYNk.EK5ttOXgP9w--`" -Lo Files_Library.7z"
+### curl.exe https://bit.ly/cskdo -Lo CSK-Download.ps1
+Write-Output "curl.exe `https://ice-eu-53814.icedrive.io/download?p=gW8Rp75a04hnL_a71tbaxDahnIfg_XZf44NmxcO4L8Jc4iOiSnL_DdrdG2OyxdwiKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWOEXuCdeefHocC66FIljxg5JJGAtRHG3dKGDJOs_2zoPNLemxjm41uzfCgNlD9g3UZNtkit57.qm0dw7wXclI25Pm6DP5HYNk.EK5ttOXgP9w--`" -Lo Files_Library.7z"

6df0ab2b7ffc723f992f6c2464fa5987753b4514
Todd Partridge <toddpartridge@outlook.com>
2024-12-18 11:22:51 -0800

    .

--- Archive-Compress.ps1
+++ Archive-Compress.ps1
@@ -67,10 +67,10 @@ cmd.exe /C dir /AL /S /B $env:SCOOP | foreach {$_.Replace("$env:USERPROFILE\",""
 Push-Location $env:USERPROFILE
 #
 # COMPRESSION ONLY (compression [low-high]: -mx=[0-9])
-7zr.exe u $PATH_CMPR -up0q0r2x1y2z1w2 -ir@"$FILE_INCS" -xr@"$FILE_EXCS" -ms=off -snh -snl -mx=0
+# 7zr.exe u $PATH_CMPR -up0q0r2x1y2z1w2 -ir@"$FILE_INCS" -xr@"$FILE_EXCS" -ms=off -snh -snl -mx=0
 #
 # CONTAINER: WIM WITH OPTIONAL COMPRESSION (COMPRESSES VERY LITTLE, ABOUT 10%).
-# 7zr.exe u $PATH_CONT -up0q0r2x1y2z1w2 -ir@"$FILE_INCS" -xr@"$FILE_EXCS" -ms=off -snh -snl
+7zr.exe u $PATH_CONT -up0q0r2x1y2z1w2 -ir@"$FILE_INCS" -xr@"$FILE_EXCS" -ms=off -snh -snl
 # 7zr.exe a $PATH_COCM $PATH_CONT -ms=off -mx5
 
 Pop-Location

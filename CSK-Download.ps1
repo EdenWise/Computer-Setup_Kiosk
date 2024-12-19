39cc821281e5b8a73cc8a1a9199b0a98bcf4f6fa
Todd Partridge <toddpartridge@outlook.com>
2024-12-19 15:16:55 -0800

    .

--- CSK-Download.ps1
+++ CSK-Download.ps1
@@ -1,12 +1,12 @@
-## Computer Setup on a Kiosk, download scripts.
+## COMPUTER SETUP ON A KIOSK, DOWNLOAD SCRIPTS.
 ### "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/master/7za.exe"
+### curl.exe https://bit.ly/cskdo -Lo CSK-Download.ps1
 
 $URIS = @(
   "https://7-zip.org/a/7zr.exe"
   #
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/Archive-Extract.ps1"
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/Computer-Setup.ps1"
-  "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/LinksVariables-Create.ps1"
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/pwsh-start.ps1"
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/pwsh-update.ps1"
   #
@@ -17,7 +17,4 @@ foreach ( $uri in $URIS ) {
   # Start-BitsTransfer -Asynchronous -Source $uri -Destination "$PWD\$(Split-Path -Leaf $uri)"
 }
 
-### curl.exe https://bit.ly/cskdo -Lo CSK-Download.ps1
-Write-Output "curl.exe `"https://ice-eu-108052.icedrive.io/download?p=ciZ6QmkI6ndmBBpr1D2aX7dktv1nitA4wIgXPcR82jdc4iOiSnL_DdrdG2OyxdwiKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWOzWv7iPGgRLRHd4kC61xTwLOSepCyZLoTqyJM4tOl.HhErMT0zv5krUe32kUZqDglNtkit57.qm0dw7wXclI25Pm6DP5HYNk.EK5ttOXgP9w--`" -Lo Files_Library.7z"
-
-echo "PSScriptAnalyzer.ps1"
+Write-Output "curl.exe `"https://icecube-eu-306.icedrive.io/download?p=8e7mJF02YX2R1adF.rspKU1m1XWeEPlnpkSb3zhay6jdoxp0Zf.8ofhOmWN5AtsjKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWNaq11P1Qoly6VK234wTnCrTv_qrID6gsl8dYRfd2Fzg6vNudA9d4GNzl6OasvqlZBNtkit57.qm0dw7wXclI25sDhZ42BVO2JdPJAXsqsRhg--`" -Lo Files_Library.7z"

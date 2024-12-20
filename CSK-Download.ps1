5575e5d7d2f0726d793189cae563c7216420e1db
Todd Partridge <toddpartridge@outlook.com>
2024-12-20 13:25:32 -0800

    Update CSK-Download.ps1

--- CSK-Download.ps1
+++ CSK-Download.ps1
@@ -1,6 +1,8 @@
 ## COMPUTER SETUP ON A KIOSK, DOWNLOAD SCRIPTS.
 ### "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/master/7za.exe"
+### Start-BitsTransfer -Asynchronous -Source $uri -Destination "$PWD\$(Split-Path -Leaf $uri)"
 ### curl.exe https://bit.ly/cskdo -Lo CSK-Download.ps1
+
 
 $URIS = @(
   "https://7-zip.org/a/7zr.exe"
@@ -14,7 +16,6 @@ $URIS = @(
 
 foreach ( $uri in $URIS ) {
   curl.exe --location --remote-name --remote-time $uri
-  # Start-BitsTransfer -Asynchronous -Source $uri -Destination "$PWD\$(Split-Path -Leaf $uri)"
 }
 
 Write-Output "curl.exe `"https://ice-us-sfo-103085.icedrive.io/download?p=r28JRABKsxF.M36Iqy3qP_aR6XK5K2L9yzM_QijWVcjdoxp0Zf.8ofhOmWN5AtsjKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWOuJYDGbmM1fR72P2W00mP5j33ZMCIiuwAg6ET70tHBPwkqgmOGnZ9bFD.1kJqnv09Ntkit57.qm0dw7wXclI25sDhZ42BVO2JdPJAXsqsRhg--`" -Lo Files_Library.wim"

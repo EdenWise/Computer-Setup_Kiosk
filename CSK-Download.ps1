b855254ad9f05adde00efa5c446bd65af1eae117
Todd Partridge <toddpartridge@outlook.com>
2024-12-28 12:58:23 -0800

    .

--- CSK-Download.ps1
+++ CSK-Download.ps1
@@ -17,4 +17,4 @@ foreach ( $uri in $URIS ) {
   curl.exe --location --remote-name --remote-time $uri
 }
 
-Write-Output "curl.exe `"https://ice-us-wdc-621142.icedrive.io/download?p=Y9uNN9lkgYg1DwVNgoGFGhiIs0uBoEr9K04uUQL0ZfTdoxp0Zf.8ofhOmWN5AtsjKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWMimWJmLUWHk5rM.9btTJmqfJH9jQUKpDpB.8ltLaTTGbdTQPtThqZ5mowTq_FVXyBNtkit57.qm0dw7wXclI25sDhZ42BVO2JdPJAXsqsRhg--`" -Lo Files_Library.wim"
+Write-Output "curl.exe `"https://ice-us-sfo-56652.icedrive.io/download?p=Y37dcDWeDmkB8pavRd3T8UXMdfHh0GwBZyXg310ywuHdoxp0Zf.8ofhOmWN5AtsjKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWNr5FbRq4_mIW8Qsrm6896720J7BBoY8ArrzfXB2HnqxrLeJwK2SZtothvVfjYavtJNtkit57.qm0dw7wXclI25sDhZ42BVO2JdPJAXsqsRhg--`" -Lo Files_Library.wim"

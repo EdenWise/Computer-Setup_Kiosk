9d5f757302b9e33359a9c7ff99ac690b44698e0a
Todd Partridge <toddpartridge@outlook.com>
2024-12-29 14:42:29 -0800

    .

--- Computer-Setup.ps1
+++ Computer-Setup.ps1
@@ -128,10 +128,11 @@ $CNFGS = @(
 foreach ( $cnfg in $CNFGS ) {
   # Test for one of three types:
   # Simple clobber replacement... please be careful with sensitive files.
-  
+  #
   # $ANSWER = Read-Host "${gitconfig}: replace with new PATH? (y/n)"
   # if ( $ANSWER -eq "y" -or $ANSWER -eq "Y") {
     #Select-String -SimpleMatch "$env:USERPROFILE" -Path $cnfg
+  Get-Content
 }
 
 

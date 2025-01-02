4f8e7c7efae5a2a7de76dbef4e26d94fb392a9fc
Todd Partridge <toddpartridge@outlook.com>
2025-01-02 12:23:54 -0800

    .

--- Files-Remove.ps1
+++ Files-Remove.ps1
@@ -1,22 +1,28 @@
 ## FILES REMOVE THAT WERE PART OF ARCHIVE.
 ###
-### Some kiosk managers may leave files on the computer. This script will remove them with the 
-### files specified to the script "Archive-Create.ps1".
+### Some kiosk managers may leave files on the computer. This script will remove
+### files left behind on the kiosk that are recorded in the 
+### `Archive-Compress.ps1 script.
+#
+# A TIP I GOT: Note when using the -Recurse parameter with -Include in Remove-Item,
+# it can be unreliable. So it's best to recurse the files first with Get-ChildItem
+# and then pipe into Remove-Item. This may also help if you deleting large folder
+# structures `Remove-Item -Recurse -Force -Path ...`
+#
 
-Push-Location $env:USERPROFILE
-
-$FILE_INCS = $env:TEMP\archive-includes.txt
+$FILE_INCS = "$env:TEMP\archive-includes.txt"
 if ( -not (Test-Path $FILE_INCS) ) {
   Write-Output "File missing: $FILE_INCS"
   exit
 }
 
-$FILE_INCS = Get-Content $FILE_INCS
+Push-Location $env:USERPROFILE
 
-# A TIP I GOT: Note when using the -Recurse parameter with -Include in Remove-Item, it can be unreliable. So it's best to recurse the files first with Get-ChildItem and then pipe into Remove-Item. This may also help if you deleting large folder structures.
-# Remove-Item -Recurse -Force -Path $FILE_INCS
-
-$FILE_INCS | Get-ChildItem -Force -Recurse | Remove-Item -Force -Recurse
+$ANSWER = Read-Host "Files remove that were extracted from the Archive."
+if ( $ANSWER -eq "y" -or $ANSWER -eq "Y") {
+  $FILE_INCS = Get-Content $FILE_INCS
+  $FILE_INCS | Get-ChildItem -Force -Recurse | Remove-Item -Force -Recurse
+}
 
 Pop-Location
 

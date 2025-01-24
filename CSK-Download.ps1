4b8af91c2e4f4d6beb196a7a0e6e846497f7e32b
Todd Partridge <toddpartridge@outlook.com>
2025-01-24 14:53:08 -0800

    .

--- CSK-Download.ps1
+++ CSK-Download.ps1
@@ -18,4 +18,4 @@ foreach ( $uri in $URIS ) {
   Start-BitsTransfer -Source $uri -Destination "$PWD\$(Split-Path -Leaf $uri)"
 }
 
-Write-Output "curl.exe `"https://ice-us-sfo-56652.icedrive.io/download?p=mAAmaOesIOm5ZcGdyAJBtn0eCOkd385mJ1T9cJGStkLdoxp0Zf.8ofhOmWN5AtsjKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWObavVY1UYFRCyAqbvtmlhXSxw83hGl9CDlgB8zfU27JDdwIzkbu9SyZWjiEYTCuq5Ntkit57.qm0dw7wXclI25sDhZ42BVO2JdPJAXsqsRhg--`" -Lo Files_Library.wim"
+Write-Output "curl.exe `"https://ice-us-wdc-621142.icedrive.io/download?p=i9.yt.E3LVLk6gLjv0Edvek4oj6_TMU39keSyVrBA_Hdoxp0Zf.8ofhOmWN5AtsjKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWP7g49Q0uIdJGww46o9B6KnAWMJotnoQhNWRci8E74KuAwIlRdewJpBR0rl7VQ6hxxNtkit57.qm0dw7wXclI25sDhZ42BVO2JdPJAXsqsRhg--`" -Lo Files_Library.wim"

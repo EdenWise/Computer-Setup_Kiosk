86ee022dabc47585f075a6dafd3702db5f092ba3
Todd Partridge <toddpartridge@outlook.com>
2025-02-09 13:37:53 -0800

    .

--- CSK-Download.ps1
+++ CSK-Download.ps1
@@ -8,14 +8,14 @@ $URIS = @(
   #
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/Archive-Extract.ps1"
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/Computer-Setup.ps1"
-  "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/pwsh-start.ps1"
   "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/pwsh-update.ps1"
+  "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/refs/heads/main/wt-start.ps1"
   #
 )
 
 foreach ( $uri in $URIS ) {
-  #curl.exe --location --remote-name --remote-time $uri
-  Start-BitsTransfer -Source $uri -Destination "$PWD\$(Split-Path -Leaf $uri)"
+  # Start-BitsTransfer -Source $uri -Destination "$PWD\$(Split-Path -Leaf $uri)"
+  curl.exe --location --remote-name --remote-time $uri
 }
 
 Write-Output "curl.exe `"https://ice-us-sfo-57081.icedrive.io/download?p=BbzEARLuiCYZ_F.FwJ4QqYVA9kL4JH4uJdXFJEAGwOLdoxp0Zf.8ofhOmWN5AtsjKtIi8OqKVJnmiYmAlgSLp7Re_EKClSVuzADjEjL5AWNQvSvyQG.dEFqizHKi159_aq4pTCkSe7H6OIX7MMcwFflzxXY6E9flKg2YDXLbYFFNtkit57.qm0dw7wXclI25sDhZ42BVO2JdPJAXsqsRhg--`" -Lo Files_Library.wim"

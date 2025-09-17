149cec28fa517feb4d808313c2e35a0b9a87e5c9
Todd Partridge <toddpartridge@outlook.com>
2025-09-17 13:32:25 -0700

    Update CSK-Download.ps1

--- CSK-Download.ps1
+++ CSK-Download.ps1
@@ -1,8 +1,6 @@
 ## COMPUTER SETUP ON A KIOSK, DOWNLOAD SCRIPTS.
 ### "https://raw.githubusercontent.com/EdenWise/Computer-Setup_Kiosk/master/7za.exe"
 ### Start-BitsTransfer -Asynchronous -Source $uri -Destination "$PWD\$(Split-Path -Leaf $uri)"
-### curl.exe https://bit.ly/cskdo -Lo CSK-Download.ps1
-### Set-ExecutionPolicy Unrestricted CurrentUser
 
 $URIS = @(
   "https://7-zip.org/a/7zr.exe"
@@ -18,4 +16,5 @@ foreach ( $uri in $URIS ) {
   # Start-BitsTransfer -Source $uri -Destination "$PWD\$(Split-Path -Leaf $uri)"
   curl.exe --location --remote-name --remote-time $uri
 }
+
 
